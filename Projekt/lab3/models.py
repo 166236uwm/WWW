2434bed583a7a6d507ef76514ed0f0fdd01c03a8
166236uwm <166236@student.uwm.edu.pl>
2024-11-21 09:24:06 +0100

    start of lab5

--- Projekt/lab3/models.py
+++ Projekt/lab3/models.py
@@ -1,4 +1,11 @@
 from django.db import models
+from datetime import datetime
+
+from prompt_toolkit.validation import ValidationError
+
+
+def contains_number(value):
+    return any(char.isdigit() for char in value)
 
 
 class Osoba(models.Model):
@@ -13,6 +20,12 @@ class Osoba(models.Model):
     plec = models.IntegerField(choices=PlecChoices.choices, default=PlecChoices.MEZCZYZNA)
     stanowisko = models.ForeignKey('Stanowisko', null=True, blank=True, on_delete=models.SET_NULL)
     data_dodania = models.DateTimeField(auto_now_add=True)
+
+    def validate(self, data_dodania):
+        if data_dodania > datetime.now():
+            raise ValidationError('Data dodania nie może być z przyszłości')
+        if contains_number(self.imie):
+            raise ValidationError('Imie nie może zawierać cyfr')
 
     def __str__(self):
         return f"{self.imie} {self.nazwisko}"
